3d76bc04cedec3f38f2bcb58aedf843112e25d12
Zattona <98216918+Zattona@users.noreply.github.com>
2022-02-27 12:31:21 +0200

    Merge argon design system

--- src/app/pages/posts/posts.module.ts
+++ src/app/pages/posts/posts.module.ts
@@ -1,25 +1,18 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-// import { AdsenseModule } from "ng2-adsense";
-import { PostsComponent } from "./posts.component";
-import { PostListingComponent } from "./post-listing/post-listing.component";
 import { RouterModule, Routes } from "@angular/router";
-import { PostService } from "./shared/post.service";
-import { PostDetailComponent } from "./post-detail/post-detail.component";
-
+import { LazyLoadImageModule } from 'ng-lazyload-image';
+import { NgxPaginationModule } from 'ngx-pagination';
 import { SharedPostModule } from "../../shared/modules/shared-post.module";
 import { SidebarModule } from "../../shared/modules/sidebar.module";
-import { ShareButtonsConfig } from 'ngx-sharebuttons';
-import { ShareButtonsModule } from 'ngx-sharebuttons/buttons';
-import { ShareIconsModule } from 'ngx-sharebuttons/icons';
-import { LazyLoadImageModule } from 'ng-lazyload-image'; 
-import {NgxPaginationModule} from 'ngx-pagination';
+import { PostDetailComponent } from "./post-detail/post-detail.component";
+import { PostListingComponent } from "./post-listing/post-listing.component";
+import { PostsComponent } from "./posts.component";
+import { PostService } from "./shared/post.service";
 
-const customConfig: ShareButtonsConfig = {
-  include: ['facebook', 'twitter', 'pinterest','whatsapp','tumblr','reddit','telegram','email'],
-  twitterAccount: 'zattona'
-}
+
+
 
 const routes: Routes = [
   {
@@ -44,14 +37,11 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FormsModule,
-    // AdsenseModule.forRoot(),
     LazyLoadImageModule,
     NgxPaginationModule,
-    ShareButtonsModule.withConfig(customConfig),
-    ShareIconsModule,
     SharedPostModule,
     SidebarModule,
     RouterModule.forChild(routes),
   ],
 })
-export class PostsModule {}
+export class PostsModule { }
